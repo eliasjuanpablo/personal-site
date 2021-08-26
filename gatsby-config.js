2a9c849101319c99806492cc8d32f57b284b44a5
juan <eliasjuanpablo1@gmail.com>
2021-08-26 12:39:36 -0300

    Improve filesystem images handling

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,16 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "gatsby-netlify",
+    siteUrl: "https://www.jpelias.com",
+    title: "jpelias",
   },
   plugins: [
-    "gatsby-plugin-netlify-cms",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/static/img`,
+        name: "uploads",
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -12,10 +18,38 @@ module.exports = {
         name: "markdown-content",
       },
     },
-    "gatsby-transformer-remark",
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-relative-images`,
+            options: {
+              name: "uploads",
+            },
+          },
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 2048,
+            },
+          },
+          {
+            resolve: "gatsby-remark-copy-linked-files",
+            options: {
+              destinationDir: "static",
+            },
+          },
+        ],
+      },
+    },
     "gatsby-plugin-styled-components",
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    "gatsby-plugin-netlify-cms",
   ],
 };
