9c910845cfbd997987aa3368b21919e0749358dc
juan <eliasjuanpablo1@gmail.com>
2021-08-30 16:28:59 -0300

    Improve 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,28 @@
 import React from "react";
+import styled from "styled-components";
+import Layout from "../components/Layout";
 
-export default () => <h1>404</h1>;
+export default () => (
+  <Layout>
+    <Wrapper>
+      <Error>404</Error>
+
+      <Description>It seems it wasn't this way.</Description>
+      <Back href="/">Get back home</Back>
+    </Wrapper>
+  </Layout>
+);
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+`;
+
+const Error = styled.div`
+  font-size: 15vmin;
+`;
+const Description = styled.div``;
+const Back = styled.a``;
